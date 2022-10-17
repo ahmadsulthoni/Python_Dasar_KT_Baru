6bd92d675e6999c408aaf931dc93a08fa2dcb951
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-10-18 03:48:38 +0700

    Type Data

---
+++ 05_Type_Data.py
@@ -0,0 +1,40 @@
+# a = 10 adalah variable dengan nilai 10
+
+# tipe data : angka satuan yang ga ada komanya (Integer)
+data_integer = 11
+print("data :", data_integer)
+print("- bertipe :", type(data_integer))
+
+# tipe data : angka dengan koma (Float)
+data_float = 1.5
+print("data :", data_float)
+print("- bertipe :", type(data_float))
+
+# tipe data : kumpulan karakter (String)
+data_string = "ucup"
+print("data :", data_string)
+print("- bertipe :", type(data_string))
+
+# tipe data : biner true/false (bool)
+data_bool = True
+print("data :", data_bool)
+print("- bertipe :", type(data_bool))
+
+## tipe data khusus
+
+# bilangan kompleks
+data_coplex = complex(5,6)
+print("data :", data_coplex)
+print("- bertipe :", type(data_coplex))
+
+# tipe data dari bahasa C
+
+
+# tipe data dari bahasa C
+
+from ctypes import c_double, c_long, c_char
+
+data_c_double = c_double(10.5)
+print("data :", data_c_double)
+print("- bertipe :", type(data_c_double))
+
