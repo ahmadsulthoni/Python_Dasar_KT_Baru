e2dc3f6ff672f1fdc874c7f9e9e69452f3f7f82e
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-10-17 20:33:12 +0700

    Pertama versi baru

---
+++ 03_Cara_Kerja_Program_&_Bytecode.py
@@ -0,0 +1,19 @@
+import time
+start_time = time.time()
+
+print ("Hello")
+print ("World")
+print ("Hello World")
+
+print ("Hello Chantik")
+#ini adalah comment
+a = 10 #ini adalah comment juga
+"""ada apa dengan 
+ucup dan otong si ganteng
+dalam comment multiline"""
+
+print(a)
+print(time.time() - start_time, "detik")
+# kita bisa mengkompile python ke yang namanya bytecode
+# cara mengkompile, buka terminal dan tuliskan 
+# python -m py_compile <nama file>.py
