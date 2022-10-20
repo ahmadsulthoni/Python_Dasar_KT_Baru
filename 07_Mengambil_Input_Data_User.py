b93f09ed4e304a1760864668ded2fea53fef9b0a
Ahmad Sulthoni <ahmadsulthoni87@gmail.com>
2022-10-20 20:27:33 +0700

    input user

---
+++ 07_Mengambil_Input_Data_User.py
@@ -0,0 +1,17 @@
+# Episode Input User
+
+# data yang di masukan pasti string
+
+data = input("Masukan data : ")
+print("data =", data , "Type",type(data))
+
+# Jika ingin mengambil int, maka
+angka = float(input("masukan angka: "))
+angka = int(input("masukan angka:"))
+
+print("data =",angka,"type =",type(angka))
+
+# bagaimana dengan boolean
+biner = bool(int(input("masukan nilai boolean:")))
+
+print("data =",biner,"type =",type(biner))
