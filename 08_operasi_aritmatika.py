11b69e2be18d5a3398b7f0e03f8119ecf2c92a39
Ahmad Sulthoni <ahmadsulthoni87@gmail.com>
2022-10-20 21:29:07 +0700

    aritmatika

---
+++ 08_operasi_aritmatika.py
@@ -0,0 +1,55 @@
+# operasi aritmatika 
+
+a = 2
+b = 3
+
+# operasi tambah (+)
+hasil = a + b
+print (a,"+",b,"=",hasil)
+
+# operasi pengurangan (-)
+hasil = a - b
+print (a,"-",b,"=",hasil)
+
+# operasi perkalian (*)
+hasil = a * b
+print (a,"x",b,"=",hasil)
+
+# operasi pembagian (/)
+hasil = a / b
+print (a,"/",b,"=",hasil)
+
+# operasi eksponen/pangkat (**)
+hasil = a ** b
+print (a,"**",b,"=",hasil)
+
+# operasi modulus/sisa (%)
+hasil = a % b
+print (a,"%",b,"=",hasil)
+
+# operasi floor division/pembulatan (//)
+hasil = a // b
+print (a,"//",b,"=",hasil)
+
+#prioritas operasi, operational precedence
+'''
+urutan 
+1. ()
+2. exponen **
+3. perkalian dan teman-temanya * / ** % //
+4. pertambahan dan pengurangan + -
+'''
+
+x = 3
+y = 2
+z = 4
+
+hasil = x ** y * (z + x) / y - y % z // x
+print(x, "**" ,y, "*" ,"(",z ,"+" ,x,")", "/" ,y, "-" ,y, "%" ,z, "//" ,x, "=", hasil)
+
+hasil = x+y*z
+print(x,"+",y,"*",z,hasil)
+
+#kurung akan mengambil langkah paling pertama
+hasil = (x+y)*z
+print ("(",x,"+",y,")""*",z,"=",hasil)
