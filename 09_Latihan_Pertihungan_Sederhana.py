a8f617a8e45c3f35e7ca7e3f20b19726d434cd6e
Ahmad Sulthoni <ahmadsulthoni.ext@gmail.com>
2022-10-22 10:01:09 +0700

    latihan perhitungan sederhana

---
+++ 09_Latihan_Pertihungan_Sederhana.py
@@ -0,0 +1,20 @@
+# latihan konfrrsi temperature
+
+# program konversi celcius ke satuan lain
+
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = float(input('masukan suhu dalam celcius: '))
+print("suhu adalah",celcius,"Celsius")
+
+# reamur
+reamur = (4/5)*celcius
+print("suhu dalam reamur adalah",reamur,"Reamur")
+
+# fahrenheit
+fahrenheit = ((9/5)*celcius) + 32
+print("suhu dalam fahrenheit adalah",fahrenheit,"Fahrenheit")
+
+# Kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin,"Kelvin")
